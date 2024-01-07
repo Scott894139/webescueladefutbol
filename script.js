b1ccb4f4494d00671b8bc17cb6dbb951079ff5eb
Scott Benavides <94999950+Scott894139@users.noreply.github.com>
2024-01-07 16:29:07 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const fulImgBox = document.getElementById("fulImgBox"),
+fulImg = document.getElementById("fulImg");
+
+function openFulImg(reference){
+    fulImgBox.style.display = "flex";
+    fulImg.src = reference
+}
+function closeImg(){
+    fulImgBox.style.display = "none";
+}
+
+
+
+let slideIndex = 1;
+showSlides(slideIndex)
+
+function plusSlides(n){
+    showSlides(slideIndex += n)
+}
+function currentSlide(n){
+    showSlides(slideIndex = n)
+}
+function showSlides(n){
+    let i;
+    let slides = document.querySelectorAll(".mySlides");
+    let quadrates = document.querySelectorAll(".quadrate"); 
+    if(n > slides.length) slideIndex = 1
+    if(n < 1) slideIndex = slides.length
+    for(i = 0; i < slides.length; i++){
+        slides[i].style.display = "none"
+    }
+    for(i = 0; i < quadrates.length;i++){
+        quadrates[i].className = quadrates[i].className.replace("active","")
+    }
+    slides[slideIndex-1].style.display = "block";
+    quadrates[slideIndex-1].className += " active";
+}
